3443b8019cc42d0a627bc9c4a96ebe697b4472ca
Jana Majed Alamer <janamijad@gmail.com>
2026-04-21 23:58:41 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
+function showLoadTime() {
+    var footer = document.querySelector("footer");
+
+    var newParagraph = document.createElement("p");
+
+    var currentDate = new Date();
+
+    var text = document.createTextNode("Page loaded on: " + currentDate.toLocaleString());
+
+    newParagraph.appendChild(text);
+
+    footer.appendChild(newParagraph);
+}
 
