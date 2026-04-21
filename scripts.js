47824ccbcf0168cfbd6fb936be7707c02bde2bbc
Jana Majed Alamer <janamijad@gmail.com>
2026-04-22 00:06:36 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 function showLoadTime() {
+    console.log("Function is working!");
+
     var footer = document.querySelector("footer");
 
     var newParagraph = document.createElement("p");
 
     var currentDate = new Date();
 
-    var text = document.createTextNode("Page loaded on: " + currentDate.toLocaleString());
+    var text = document.createTextNode("Page loaded on: " + currentDate);
 
     newParagraph.appendChild(text);
 
     footer.appendChild(newParagraph);
 }
+
+window.onload = showLoadTime;
 
