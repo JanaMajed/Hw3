de7f878b53199964efcf0f0831b253225f790560
Jana Majed Alamer <janamijad@gmail.com>
2026-04-22 00:44:45 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -55,3 +55,16 @@ function toggleDescription(id) {
         desc.style.display = "none";
     }
 }
+
+
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+    if (name === "" || email === "") {
+        alert("Please fill out all required fields.");
+        return false; 
+    }
+
+    return true; 
+}
